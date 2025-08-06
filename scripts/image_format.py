83768766af017ef74169cc391cc93504bf676738
Mira Sohn <tonton07@gmail.com>
2025-08-06 00:03:26 -0400

    input paths updates to the python temp file

--- scripts/image_format.py
+++ scripts/image_format.py
@@ -10,23 +10,40 @@ outdir = "../images/converted"
 os.makedirs(outdir, exist_ok=True)
 
 bfc = "../bftools/bfconvert"
+qupath = "../bftools/bfconvert"
 
-im0 = "../images/2025_05_12_121824A_MCTX_FFPE_IBA1_TDP43_MAP2_ALDH1L1/2025_05_12_121824A_MCTX_FFPE_IBA1_TDP43_MAP2_ALDH1L1_Perm+RNAseInhib/Image_168.vsi"
-im1 = "../images/2025_05_12_121824A_MCTX_FFPE_IBA1_TDP43_MAP2_ALDH1L1/2025_05_12_121824A_MCTX_FFPE_IBA1_TDP43_MAP2_ALDH1L1_Perm+RNAseInhib/tiffzoom/Image_01.vsi"
-im2 = "../images/2025_05_12_121824A_MCTX_FFPE_IBA1_TDP43_MAP2_ALDH1L1/2025_05_12_121824A_MCTX_FFPE_IBA1_TDP43_MAP2_ALDH1L1_Perm+RNAseInhib/tiffzoom/Image_02.vsi"
+im0 = "../images/NoPerm_RNAseInhib_20250512/Image_169.vsi"
+im1 = "../images/Perm_RNAseInhib_20250512/Image_168.vsi"
+im2 = "../images/NoPerm_RNAseInhib_20250512/tiffzoom/Image_02.vsi"
+im3 = "../images/Perm_RNAseInhib_20250512/tiffzoom/Image_01.vsi"
 
-img_dic = {'im0' : {'in': im0}, 'im1' : {'in' : im1}, 'im2' : {'in' : im2}}
+img_dic = {'im0' : {'in': im0}, 'im1' : {'in' : im1}, 'im2' : {'in' : im2}, 'im3' : {'in' : im3}}
 
 for key, value in img_dic.items():
     value['out'] = f"{outdir}/{key}.tif" 
 
-In [33]: print(img_dic)
-{'im0': 
- {'in': '../images/2025_05_12_121824A_MCTX_FFPE_IBA1_TDP43_MAP2_ALDH1L1/2025_05_12_121824A_MCTX_FFPE_IBA1_TDP43_MAP2_ALDH1L1_Perm+RNAseInhib/Image_168.vsi', 'out': '../images/converted/im0.tif'}, 
- 'im1': {'in': '../images/2025_05_12_121824A_MCTX_FFPE_IBA1_TDP43_MAP2_ALDH1L1/2025_05_12_121824A_MCTX_FFPE_IBA1_TDP43_MAP2_ALDH1L1_Perm+RNAseInhib/tiffzoom/Image_01.vsi', 'out': '../images/converted/im1.tif'}, 
- 'im2': {'in': '../images/2025_05_12_121824A_MCTX_FFPE_IBA1_TDP43_MAP2_ALDH1L1/2025_05_12_121824A_MCTX_FFPE_IBA1_TDP43_MAP2_ALDH1L1_Perm+RNAseInhib/tiffzoom/Image_02.vsi', 'out': '../images/converted/im2.tif'}}
+input_image = 'im0'
+cmd = ["vsi2tif -i", img_dic[input_image]['in'], "-o", img_dic[input_image]['out'], "-b", bfc]
+cmd = " ".join(cmd)
+subprocess.run(cmd, shell=True)
 
 input_image = 'im1'
-cmd0 = ["vsi2tif -i", img_dic[input_image]['in'], "-o", img_dic[input_image]['out'], "-b", bfc]
+cmd = ["vsi2tif -i", img_dic[input_image]['in'], "-o", img_dic[input_image]['out'], "-b", bfc]
+cmd = " ".join(cmd)
+subprocess.run(cmd, shell=True)
+
+input_image = 'im2'
+cmd = ["vsi2tif -i", img_dic[input_image]['in'], "-o", img_dic[input_image]['out'], "-b", bfc]
+cmd = " ".join(cmd)
+subprocess.run(cmd, shell=True)
+
+input_image = 'im3'
+cmd = ["vsi2tif -i", img_dic[input_image]['in'], "-o", img_dic[input_image]['out'], "-b", bfc]
+cmd = " ".join(cmd)
+subprocess.run(cmd, shell=True)
+
+
+
+
 if not os.path.exists(img_dic[input_image]['out']):
-    subprocess.run(" ".join(cmd0), shell=True)
+    subprocess.run(, shell=True)
