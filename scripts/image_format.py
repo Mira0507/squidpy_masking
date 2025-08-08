dc90c4ca889125d3e259723e0577f5af548a456d
Mira Sohn <tonton07@gmail.com>
2025-08-08 11:13:36 -0400

    image format conversion in progress

--- scripts/image_format.py
+++ scripts/image_format.py
@@ -4,46 +4,32 @@ import squidpy as sq
 import napari
 import os
 import subprocess
+import time
 
 
-outdir = "../images/converted"
+outdir = "../images/output"
 os.makedirs(outdir, exist_ok=True)
 
-bfc = "../bftools/bfconvert"
-qupath = "../bftools/bfconvert"
+cachedir = "image_format_cache"
 
-im0 = "../images/NoPerm_RNAseInhib_20250512/Image_169.vsi"
-im1 = "../images/Perm_RNAseInhib_20250512/Image_168.vsi"
-im2 = "../images/NoPerm_RNAseInhib_20250512/tiffzoom/Image_02.vsi"
-im3 = "../images/Perm_RNAseInhib_20250512/tiffzoom/Image_01.vsi"
+im0 = "../images/input/NoPerm/Image_169.vsi"
+im1 = "../images/input/Perm/Image_168.vsi"
 
-img_dic = {'im0' : {'in': im0}, 'im1' : {'in' : im1}, 'im2' : {'in' : im2}, 'im3' : {'in' : im3}}
+cmd = f"bfconvert -noflat -compression LZW -overwrite -cache -cache-dir {cachedir} {im0} {outdir}/converted_169.ome.tif"
 
-for key, value in img_dic.items():
-    value['out'] = f"{outdir}/{key}.tif" 
+def run_runtime(): 
+    start_time = time.time()
+    subprocess.run(cmd, shell=True)
+    end_time = time.time()
+    run_time_sec = end_time - start_time
+    run_time_min = run_time_sec / 60
+    print(f"Runtime: {run_time_sec} seconds ({run_time_min} minutes).")
 
-input_image = 'im0'
-cmd = ["vsi2tif -i", img_dic[input_image]['in'], "-o", img_dic[input_image]['out'], "-b", bfc]
-cmd = " ".join(cmd)
-subprocess.run(cmd, shell=True)
+run_runtime()
 
-input_image = 'im1'
-cmd = ["vsi2tif -i", img_dic[input_image]['in'], "-o", img_dic[input_image]['out'], "-b", bfc]
-cmd = " ".join(cmd)
-subprocess.run(cmd, shell=True)
-
-input_image = 'im2'
-cmd = ["vsi2tif -i", img_dic[input_image]['in'], "-o", img_dic[input_image]['out'], "-b", bfc]
-cmd = " ".join(cmd)
-subprocess.run(cmd, shell=True)
-
-input_image = 'im3'
-cmd = ["vsi2tif -i", img_dic[input_image]['in'], "-o", img_dic[input_image]['out'], "-b", bfc]
-cmd = " ".join(cmd)
-subprocess.run(cmd, shell=True)
+# In [36]: print(cmd)
+# bfconvert -noflat -compression LZW -overwrite -cache -cache-dir image_format_cache ../images/input/NoPerm/Image_169.v
+# si ../images/output/converted_169.ome.tif
 
 
 
-
-if not os.path.exists(img_dic[input_image]['out']):
-    subprocess.run(, shell=True)
